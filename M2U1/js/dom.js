87732a604ca66ec9e458c93569dce51ef9617f33
Emiliano Ramos <vertex.grafica@gmail.com>
2024-12-05 11:14:35 -0300

    Agregado de ejercicio 2

---
+++ M2U1/js/dom.js
@@ -0,0 +1,15 @@
+function cambiarTexto() {
+    let parrafo = document.getElementById("cambioDeTexto");
+    parrafo.textContent = "Has cambiado el texto!!!";
+    
+}
+
+function cambiarColor() {
+    let parrafo = document.getElementById("cambioDeColor");
+    parrafo.style.backgroundColor = "red";
+}
+
+function cambiarColorTexto() {
+    let parrafo = document.getElementById("colorDeTexto");
+    parrafo.style.color = "green";
+}
